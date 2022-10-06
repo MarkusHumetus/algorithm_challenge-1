36dedd895b5062bc493b5694ff779b9527ff3d6c
Marc Humet <123kkipp@gmail.com>
2022-10-06 16:11:28 +0200

    algorythm finished

---
+++ Algorythm_challenge_1.py
@@ -0,0 +1,31 @@
+def divisor (num_tot, divisor):
+    '''funtion to check wheter a number is a divisor of another
+            INPUT: 2 numbers (first one is > 2nd one)
+            OUTPUT: True or False depending if is divisor or not.'''
+
+    if num_tot%divisor==0:
+        return True
+    else:
+        return False
+
+
+check =False
+i = 20*19*17*13*11
+while check ==False:
+    i  +=1
+    if divisor(i,20):
+        if divisor(i,19):
+            if divisor(i,18):
+                if divisor(i,17):
+                    if divisor(i,16):
+                        if divisor(i,15):
+                            if divisor(i,14):
+                                if divisor(i,13):
+                                    if divisor(i,12):                                        
+                                        if divisor(i,11):
+                                            check = True
+
+print ('* ' *55,'\n')
+rint (f'The smallest number that cais divisible by all numbers from 20 to 1 (both included) is {i}.\n')
+print ('* ' *55,'\n')
+
